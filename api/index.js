5b44f2c5f771da38275ee22d2a8d652c4cdd8cc2
SylanTroh <lauranemeth11@gmail.com>
2025-07-03 01:15:43 -0400

    Prepare for service transfer

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
@@ -10,16 +11,90 @@ const backupPath = 'api/saveBricks.txt';
 
 const app = express();
 
-//Show Current Bricks and add One
+// General rate limiter
+const generalLimiter = rateLimit({
+    windowMs: 14 * 1000,
+    max: 3, // Limit each IP to 3 requests per 14 seconds
+    message: {
+        error: 'Too many requests from this IP, please try again later.',
+        retryAfter: '15 seconds'
+    },
+    standardHeaders: true,
+    legacyHeaders: false, 
+    keyGenerator: (req) => {
+        return req.ip;
+    }
+});
+
+// Apply general rate limiting to all routes
+app.use(generalLimiter);
+
+// Write queue to handle race conditions
+class WriteQueue {
+    constructor() {
+        this.queue = [];
+        this.isProcessing = false;
+    }
+
+    async enqueue(operation) {
+        return new Promise((resolve, reject) => {
+            this.queue.push({ operation, resolve, reject });
+            this.process();
+        });
+    }
+
+    async process() {
+        if (this.isProcessing || this.queue.length === 0) {
+            return;
+        }
+
+        this.isProcessing = true;
+
+        while (this.queue.length > 0) {
+            const { operation, resolve, reject } = this.queue.shift();
+
+            try {
+                const result = await operation();
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        }
+
+        this.isProcessing = false;
+    }
+}
+
+const writeQueue = new WriteQueue();
+
+//Show Current Bricks
 app.get('/', (req, res) => {
-    SuccessResponse(bricks,res,req);
+    SuccessResponseBricks(bricks,res,req);
+});
+
+//Show Server Time
+app.get('/time', (req, res) => {
+    SuccessResponseTime(res,req);
 });
 
 //Add One Brick
 app.get('/place/:id', (req, res) => {
-    if(CheckOTP(req.params.id)) { PlaceBrick(res,req);}
+    if(CheckOTP(req.params.id)) { PlaceBrick(1,res,req);}
     else { FailureResponse(res,req);}
 });
+
+//Add Two Bricks
+app.get('/place2/:id', (req, res) => {
+    if(CheckOTP(req.params.id)) { PlaceBrick(2,res,req);}
+    else { FailureResponse(res,req);}
+});
+
+//Add Three Bricks
+app.get('/place3/:id', (req, res) => {
+    if(CheckOTP(req.params.id)) { PlaceBrick(3,res,req);}
+    else { FailureResponse(res,req);}
+});
+
 
 function startServer() {
     const servers = [];
@@ -58,18 +133,28 @@ function startServer() {
 
     return servers;
 }
-function SuccessResponse(bricks,res,req){
+
+function SuccessResponseBricks(bricks, res, req) {
+    res.status(200).send(bricks.toString());
+}
+
+function SuccessResponseTime(res, req) {
     const d = new Date();
-    let time = d.getTime()
-    res.send(
-        bricks.toString()+","+
-        time.toString()
-    );
+    let time = d.getTime();
+    res.status(200).send(time.toString());
 }
 
 function FailureResponse(res,req){
     console.log(`Error: Incorrect otp: ${req.params.id}`);
-    res.send("Error");
+    res.status(400).send("<p>Hi! This is Sylan!<br>" +
+        "If you're here, that means you're poking around at the server for my tower world.<br>" +
+        "It's pretty cool that you've managed to get here, I myself love to tinker around with things and figure out how they work, but I would like to please ask you to consider the following before poking any further:<br>" +
+        "I made this world because I wanted to make a fun experience that brings people together.<br>" +
+        "To accomplish this, I pay for the server that makes this possible out of pocket, and I am more than willing to do so because it makes me happy to see others having fun.<br>" +
+        "I don't make any money from this world. This means that if bots or users outside VR decide to flood the server with requests, I will likely not be able to afford to keep it running anymore, shutting it down for everyone.<br>" +
+        "While I can't possibly know your intentions, I hope you can keep that in mind if you decide to continue poking around here.<br>" +
+        "<br>" +
+        "- Sylan</p>");
 }
 
 function MD5Hash(str) {
@@ -98,45 +183,85 @@ function CheckOTP(otp){
     return (otp == currentOTP) || (otp == prevOTP)
 }
 
-function PlaceBrick(res,req)
-{
-    bricks += 1;
-    fs.writeFile(dataPath, bricks.toString(), err => {
-        if (err) {
-            console.error('Error writing to file:', err);
-            FailureResponse(res,req);
-        }
-        else {
-            console.log(`Placed Brick ${bricks} on ID: ${req.params.id}`);
-            SuccessResponse(bricks, res, req);
-        }
-    });
+async function PlaceBrick(num, res, req) {
+    try {
+        await writeQueue.enqueue(async () => {
+            // Increment bricks in memory
+            bricks += num;
+
+            // Write to file using promises
+            return new Promise((resolve, reject) => {
+                fs.writeFile(dataPath, bricks.toString(), err => {
+                    if (err) {
+                        // Rollback the in-memory change on error
+                        bricks -= num;
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        });
+
+        console.log(`Placed ${num} brick(s). Total: ${bricks} on ID: ${req.params.id}`);
+        SuccessResponseBricks(bricks, res, req);
+    } catch (error) {
+        console.error('Error placing brick:', error);
+        FailureResponse(res, req);
+    }
 }
 
-function SaveBricks(){
-    fs.appendFile(backupPath, bricks.toString() + `\n`, err => {
-        if (err) {
-            console.error('Error writing to file:', err);
-        }
-        else {
-            console.log(`Logged ${bricks} bricks`);
-        }
-    });
+async function SaveBricks(){
+    try {
+        await writeQueue.enqueue(async () => {
+            return new Promise((resolve, reject) => {
+                const timestamp = new Date().toISOString();
+                const backupEntry = `${timestamp}: ${bricks}\n`;
+                
+                fs.appendFile(backupPath, backupEntry, err => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        });
+
+        console.log(`Logged ${bricks} bricks`);
+    } catch (error) {
+        console.error('Error saving bricks:', error);
+    }
 }
 
 /// Console Prompt
 
 const commands = {
-    setbricks: (number) => {
-        bricks = parseInt(number);
-        fs.writeFile(dataPath, bricks.toString(), err => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            }
-            else {
-                console.log(`Set bricks to ${bricks}`);
-            }
-        });
+    setbricks: async (number) => {
+        const newBricks = parseInt(number);
+        if (isNaN(newBricks)) {
+            console.log('Invalid number provided');
+            return;
+        }
+
+        try {
+            await writeQueue.enqueue(async () => {
+                bricks = newBricks;
+                return new Promise((resolve, reject) => {
+                    fs.writeFile(dataPath, bricks.toString(), err => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                });
+            });
+
+            console.log(`Set bricks to ${bricks}`);
+        } catch (error) {
+            console.error('Error setting bricks:', error);
+        }
     },
 
     time: () => {
